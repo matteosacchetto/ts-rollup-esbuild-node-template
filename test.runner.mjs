98f6069535a642a45a9ad6ce6d426dbfd8d46276
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2024-01-09 11:37:26 +0100

    test(runner): add option -p,--parallel to run test in parallel

--- test.runner.mjs
+++ test.runner.mjs
@@ -3,6 +3,19 @@ import { spec, tap } from 'node:test/reporters';
 import { readdir } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import { parseArgs } from 'node:util';
+
+const {
+  values: { parallel },
+} = parseArgs({
+  options: {
+    parallel: {
+      type: 'boolean',
+      short: 'p',
+      default: false,
+    },
+  },
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const dir = join(__dirname, 'test');
@@ -11,6 +24,6 @@ const files = (await readdir(dir, { recursive: true }))
   .filter((el) => el.endsWith('.test.ts'))
   .map((el) => join(dir, el));
 
-run({ files })
+run({ files, concurrency: parallel })
   .compose(process.stdout.isTTY ? new spec() : tap)
   .pipe(process.stdout);
